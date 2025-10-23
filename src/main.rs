92744a1b14a74695f094bde6216b2c07ac416fea
Wil Cram <wcram17@gmail.com>
2025-10-23 17:27:55 -0500

    finish up classical lambda calculus

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,64 @@ fn fresh_var(base: &str, in_use: &HashSet<String>) -> String {
     name
 }
 
+// Safely substitues t[x -> s].
+fn subst(t: &Term, x: &str, s: &Term) -> Term {
+    match t {
+        Term::Var(y) => {
+            if x == y {
+                s.clone()
+            } else {
+                t.clone()
+            }
+        }
+        Term::Abs(y, body) => {
+            // shadowed case
+            if y == x {
+                t.clone()
+            } else if free_vars(s).contains(y) {
+                let used: HashSet<_> = free_vars(body).union(&free_vars(s)).cloned().collect();
+                let fresh = fresh_var(y, &used);
+                let renamed_body = subst(body, y, &Term::Var(fresh.clone()));
+                Term::Abs(fresh, Box::new(subst(&renamed_body, x, s)))
+            } else {
+                Term::Abs(y.clone(), Box::new(subst(body, x, s)))
+            }
+        }
+        Term::App(a, b) => Term::App(Box::new(subst(a, x, s)), Box::new(subst(b, x, s))),
+    }
+}
+
+// Steps this term one time, returning None if irreducible (we don't reduce under lambdas)
+fn small_step(t: &Term) -> Option<Term> {
+    match t {
+        Term::Var(_) => None,
+        Term::Abs(x, body) => None,
+        Term::App(a, b) => match &**a {
+            Term::Abs(x, body) => Some(subst(body, x, b)),
+            _ => {
+                if let Some(a2) = small_step(a) {
+                    Some(Term::App(Box::new(a2), b.clone()))
+                } else if let Some(b2) = small_step(b) {
+                    Some(Term::App(a.clone(), Box::new(b2)))
+                } else {
+                    None
+                }
+            }
+        },
+    }
+}
+
+fn normalize(mut t: Term, limit: usize) -> Result<Term, String> {
+    for _ in 0..limit {
+        if let Some(next) = small_step(&t) {
+            t = next;
+        } else {
+            return Ok(t);
+        }
+    }
+    Err(format!("couldn't find normal form: {t}"))
+}
+
 impl std::fmt::Display for Term {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -38,6 +96,20 @@ impl std::fmt::Display for Term {
 }
 
 fn main() {
+    let TRUE = Term::Abs(
+        "x".into(),
+        Box::new(Term::Abs("y".into(), Box::new(Term::Var("x".into())))),
+    );
+    println!("TRUE = {}", TRUE);
+    println!(
+        "{}",
+        normalize(
+            Term::App(Box::new(TRUE), Box::new(Term::Var("x".into()))),
+            1000
+        )
+        .unwrap()
+    );
+
     let omega = Term::App(
         Box::new(Term::Abs(
             "x".into(),
@@ -55,4 +127,5 @@ fn main() {
         )),
     );
     println!("Ω = {}", omega);
+    println!("{}", normalize(omega, 1000).unwrap_err());
 }
